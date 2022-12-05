fc16b1cc4f2ae5667ee593e62075b23a7ba3b025
sheetukohli <119625444+sheetukohli@users.noreply.github.com>
2022-12-04 21:25:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 if not fruit_choice: 
     streamlit.error("Please select a fruity to get information")
-else
+else:
     #streamlit.write('The user entered ', fruit_choice)
 
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"Kiwi")
